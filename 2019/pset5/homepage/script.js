24790bd9ea64ef25884d07a588033cc55f2c3385
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2020-04-15 00:58:42 +0200

    fixed myRecentWorkBox images heights

--- 2019/pset5/homepage/script.js
+++ 2019/pset5/homepage/script.js
@@ -4,6 +4,24 @@ $('.navbar-toggler').click(function () {
 
 
 $('.container').find('.myRecentWorkBox').find('img').each(function () {
-    var imgClass = (this.width / this.height > 1) ? 'wide' : 'tall';
+    let imgClass = (this.width / this.height > 1) ? 'wide' : 'tall';
     $(this).addClass(imgClass);
 });
+
+
+function setMyRecentWorkBoxHeight()
+{
+    let MyRecentWorkBox = document.getElementsByClassName("myRecentWorkBox");
+    let MyRecentWorkBoxWidth = MyRecentWorkBox[0].clientWidth;
+    console.log(MyRecentWorkBoxWidth);
+    // let tempVar = document.getElementsByClassName('myRecentWorkBox');
+    for(let i=0; i<MyRecentWorkBox.length; i++)
+    {
+        MyRecentWorkBox[i].style.height = `${MyRecentWorkBoxWidth}px`;
+    }
+    // tempVar.style.height = MyRecentWorkBoxWidth;
+
+}
+
+setMyRecentWorkBoxHeight();
+window.addEventListener("resize",setMyRecentWorkBoxHeight);
