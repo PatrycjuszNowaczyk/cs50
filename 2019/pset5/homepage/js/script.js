bb9eb3c69d40500eabcaedfde791bcecc74751e5
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2020-04-29 23:44:09 +0200

    styled contact form

--- 2019/pset5/homepage/js/script.js
+++ 2019/pset5/homepage/js/script.js
@@ -1,7 +1,7 @@
+
 $('.navbar-toggler').click(function () {
     $('.burger').toggleClass('open');
 });
-
 
 $('.container').find('.myRecentWorkBox').find('img').each(function () {
     let imgClass = (this.width / this.height > 1) ? 'wide' : 'tall';
@@ -9,14 +9,13 @@ $('.container').find('.myRecentWorkBox').find('img').each(function () {
 });
 
 
-function setMyRecentWorkBoxHeight()
-{
+
+function setMyRecentWorkBoxHeight() {
     let MyRecentWorkBox = document.getElementsByClassName("myRecentWorkBox");
     let MyRecentWorkBoxWidth = MyRecentWorkBox[0].clientWidth;
     console.log(MyRecentWorkBoxWidth);
     // let tempVar = document.getElementsByClassName('myRecentWorkBox');
-    for(let i=0; i<MyRecentWorkBox.length; i++)
-    {
+    for (let i = 0; i < MyRecentWorkBox.length; i++) {
         MyRecentWorkBox[i].style.height = `${MyRecentWorkBoxWidth}px`;
     }
     // tempVar.style.height = MyRecentWorkBoxWidth;
@@ -24,4 +23,17 @@ function setMyRecentWorkBoxHeight()
 }
 
 setMyRecentWorkBoxHeight();
-window.addEventListener("resize",setMyRecentWorkBoxHeight);
+window.addEventListener("resize", setMyRecentWorkBoxHeight);
+
+function addAutoResize() {
+    document.querySelectorAll('[data-autoresize]').forEach(function (element) {
+        element.style.boxSizing = 'border-box';
+        var offset = element.offsetHeight - element.clientHeight;
+        document.addEventListener('input', function (event) {
+            event.target.style.height = 'auto';
+            event.target.style.height = event.target.scrollHeight + offset + 'px';
+        });
+        element.removeAttribute('data-autoresize');
+    });
+}
+
